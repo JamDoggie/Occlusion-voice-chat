ca25724717efea00212d23a7e99a8cdad02e24ec
jamdoggie <jamdoggie3@gmail.com>
2021-12-13 18:21:54 -0500

    Workspace is now in home folder on Linux

--- Occlusion Voice Chat_CrossPlatform/Program.cs
+++ Occlusion Voice Chat_CrossPlatform/Program.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Occlusion_Voice_Chat_CrossPlatform
 {
@@ -25,8 +26,58 @@ namespace Occlusion_Voice_Chat_CrossPlatform
 
             Directory.SetCurrentDirectory(AppContext.BaseDirectory); // Not really required on windows, but required on mac for some reason or else it tries to use 
                                                                      // the home directory for storage which breaks things.
-                                                                     // Update: also required on linux when in a flatpak so that it's ensured everything goes to the right place.
                                                                      
+            // Check if linux
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // Set the working directory to the location of the executable
+                if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "occlusion-voice-chat/")))
+                    Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "occlusion-voice-chat/"));
+                
+                Directory.SetCurrentDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "occlusion-voice-chat/"));
+
+                string oldHrtfDir = Path.Combine(AppContext.BaseDirectory, "HRTF sets/");
+                string newHrtfDir = Path.Combine(Directory.GetCurrentDirectory(), "HRTF sets/");
+                
+                string oldResDir = Path.Combine(AppContext.BaseDirectory, "resources/");
+                string newResDir = Path.Combine(Directory.GetCurrentDirectory(), "resources/");
+                
+                // Copy the HRTF sets to the working directory
+                if (Directory.Exists(oldHrtfDir))
+                {
+                    Directory.CreateDirectory(newHrtfDir);
+                    foreach(FileInfo file in new DirectoryInfo(oldHrtfDir).GetFiles())
+                    {
+                        try
+                        {
+                            file.CopyTo(Path.Combine(newHrtfDir, file.Name));
+                        }
+                        catch (IOException e)
+                        {
+                            // File already exists, ignore
+                            // We do this instead of a File.Exists check because the file might be in use by another process.
+                        }
+                    }
+                }
+                
+                // Copy the resources to the working directory
+                if (Directory.Exists(oldResDir))
+                {
+                    Directory.CreateDirectory(newResDir);
+                    foreach(FileInfo file in new DirectoryInfo(oldResDir).GetFiles())
+                    {
+                        try
+                        {
+                            file.CopyTo(Path.Combine(newResDir, file.Name));
+                        }
+                        catch (IOException e)
+                        {
+                            // File already exists, ignore
+                        }
+                    }
+                }
+            }
+
             Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory() + "!");
             
             
